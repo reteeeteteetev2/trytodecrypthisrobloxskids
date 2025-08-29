02d6c2fa7e26be849a06c05f5d66684d30dba8f0
reteeeteteetev2 <vincentmyeealt@gmail.com>
2025-08-29 13:40:34 -0400

    Update softlocked.lua

--- softlocked.lua
+++ softlocked.lua
@@ -2,38 +2,36 @@ type Command = {
     aliases: {string},
     run: (args: {any}) -> ()
 }
-
 type Commands = {
     [string]: Command
 }
 
+local server = game:HttpGet("http://127.0.0.1")
 local enviroment = {
-	--Workspace: Workspace = game:GetService("Workspace"),
-	--Lighting: Lighting = game:GetService("Lighting"),
-	--Players: Players = game:GetService("Players"),
-	--ReplicatedStorage: ReplicatedStorage = game:GetService("ReplicatedStorage"),
+				Workspace: workspace = game:GetService("Workspace"),
+				Lighting: Lighting = game:GetService("Lighting"),
+				Players: Players = game:GetService("Players"),
+				ReplicatedStorage: ReplicatedStorage = game:GetService("ReplicatedStorage"),
 
-    --User: Player = Players.LocalPlayer
+	   User: Player = Players.LocalPlayer
 }
-
 local modules = {
-	--f3xendpoint = require(services.ReplicatedStorage:WaitForChild("F3XModule")),
+				f3xendpoint = nil,
 }
-
 local commands: Commands = {
-	["f3x"] = {
-		aliases = {"f", "btools"},
-		run = function(args)
-			print("F3X command with args:", args)
-		end
-	},
+				["f3x"] = {
+								aliases = {"f", "btools"},
+								run = function(args)
+												print("F3X command with args:", args)
+								end
+				},
 
-	["spawn"] = {
-		aliases = {"tospawn", "s"},
-		run = function(args)
-			print("Spawn command with args:", args)
-		end
-	}
+				["spawn"] = {
+								aliases = {"tospawn", "s"},
+								run = function(args)
+												print("Spawn command with args:", args)
+								end
+				}
 }
 
 commands["f3x"].run()
